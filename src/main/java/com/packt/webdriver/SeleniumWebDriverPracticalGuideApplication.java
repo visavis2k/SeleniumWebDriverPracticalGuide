92358ccfbe0a8982a9e603b893bab383ae680fe6
Kyutae Park <visavis2k@gmail.com>
2015-07-07 06:21:19 +0900

    initial commit

---
+++ src/main/java/com/packt/webdriver/SeleniumWebDriverPracticalGuideApplication.java
@@ -0,0 +1,12 @@
+package com.packt.webdriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumWebDriverPracticalGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeleniumWebDriverPracticalGuideApplication.class, args);
+    }
+}
